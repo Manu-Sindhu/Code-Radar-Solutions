478e19189f5e340251fe03919d0beadbef40b04f
Manu-Sindhu <manu1410.becse24@chitkara.edu.in>
2025-02-12 18:06:21 +0530

    Solution to Perform Bitwise Left Shift

--- Perform Bitwise Left Shift@@67882a731b4392d8fc1577e5/code.c
+++ Perform Bitwise Left Shift@@67882a731b4392d8fc1577e5/code.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-int main(){
-    int a;
-    scanf("%d",&a);
-    int result =a<<2;
-    printf("%d",result);
-    return 0;
+
+int main() {
+    int a;  
+    scanf("%d", &a);  // Take input from the user
+
+    int result = a << 2;  // Left shift 'a' by 2 positions
+
+    printf("%d", result);  // Print the result
+
+    return 0;  
 }
+
