729e4f04c29f127bdabd1c39cae044cfd03f7874
Manu-Sindhu <manu1410.becse24@chitkara.edu.in>
2025-03-07 16:59:01 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -3,8 +3,20 @@ int main(){
     int a;
     scanf("%c",&a);
     switch(a){
-        case A:
+        case 'A':
         printf("Excellent");
+        break;
+        case 'B':
+        printf("Good");
+        break;
+        case 'C':
+        printf("Average");
+        break;
+        case 'D':
+        printf("Below Averaage");
+        break;
+        case 'F':
+        printf("Fail");
         break;
     }
 }
